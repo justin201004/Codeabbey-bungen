07174526bbc3ec1c2ed2f19e15df05b86806a302
Justin Witt <Justin Witt@DESKTOP-D2SGNUM>
2021-08-04 08:33:44 +0200

    Projektdateien hinzufügen.

---
+++ lernen/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace lernen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Eingabe der ersten Zahl:");
+
+
+             int eingabe1 = Convert.ToInt32 (Console.ReadLine());
+
+             Console.WriteLine("Eingabe der zweiten Zahl:");
+
+             int eingabe2 = Convert.ToInt32(Console.ReadLine());
+
+             Console.WriteLine(eingabe2 + eingabe1);*/
+
+
+
+
+
+
+
+
+
+            //Aufgabe 2
+
+
+            /*Console.WriteLine("Wie viele Zahlen willst du addieren?!");
+
+            int anzahl = Convert.ToInt32(Console.ReadLine());
+
+            int result = 0;
+
+            for (int i = 1; i <= anzahl; i++)
+            {
+                Console.WriteLine("Eingabe der "+i+" Zahl!");
+
+                int eingabe1 = Convert.ToInt32(Console.ReadLine());
+
+                 result = eingabe1 + result;
+
+
+            }Console.WriteLine(result);*/
+
+
+            /*Console.WriteLine("Wie viele Zahlen willst du addieren?!");
+
+
+                        int anzahl = Convert.ToInt32(Console.ReadLine());
+
+                        int result = 0;
+
+            for (int x = 0; x < anzahl; x++)
+            {
+                int [,] parship = new int[2,anzahl];
+
+                for (int i = 0; i <= anzahl; i++)
+                {
+                    Console.WriteLine("Eingabe der  Zahl!");
+
+                    int eingabe1 = Convert.ToInt32(Console.ReadLine());
+                    eingabe1 = parship[i, ;
+
+
+
+
+                    result = eingabe1 + result;
+
+
+
+                }
+                Console.WriteLine(result);
+
+
+            }*/
+
+            /*Console.WriteLine("Wie viele Zahlen willst du addieren?!");
+
+
+            int anzahl = Convert.ToInt32(Console.ReadLine());
+
+            int[] result = new int[anzahl];
+
+            for (int i = 0; i < anzahl; i++)
+            {
+                string input = Console.ReadLine();
+                var inputNumbers = input.Split(' ');
+                int firstNumber = Convert.ToInt32(inputNumbers[0]);
+                int secondNumber = Convert.ToInt32(inputNumbers[1]);
+
+                result[i] = firstNumber + secondNumber;
+
+
+
+
+            }
+
+            Console.WriteLine("answer:");
+            Console.WriteLine(string.Join(' ', result));
+
+        */
+        }
+    }
+}
+
